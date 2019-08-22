026bc4ca1f63d5e313c324805338ef3ef7b7f186
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-22 04:20:59 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,6 @@
 class Cat 
-  attr_accessor :name
+  attr_accessor :name #adds the writer and reader for name
   def meow 
-    puts "meow!"
+    puts "meow!" #teaches every new cat you make to be able to meow
   end
 end
